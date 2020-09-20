fb081da55b7b5517627f03ebffb6ee9c4c33ae90
tal <alfital2@gmail.com>
2020-09-20 22:18:50 +0300

    writing the mathmatical-functions - first version!

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,31 @@
-let currentResult = 0 ;
-currentResult += 10;
+const defaultResult = 0 ;
+let currentResult = defaultResult ;
+let calculationDescription = '';
 
-outputResult(currentResult,'');
+
+function add(){
+  currentResult = currentResult + parseInt( userInput.value );
+  outputResult(currentResult,calculationDescription);
+}
+
+function subtract(){
+  currentResult = currentResult - parseInt( userInput.value );
+  outputResult(currentResult,calculationDescription);
+}
+
+function mul(){
+  currentResult = currentResult * parseInt( userInput.value );
+  outputResult(currentResult,calculationDescription);
+}
+
+function div(){
+  currentResult = currentResult / parseInt( userInput.value );
+  outputResult(currentResult,calculationDescription);
+}
+
+
+
+addBtn.addEventListener('click',add);
+subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click',mul);
+divideBtn.addEventListener('click',div);
